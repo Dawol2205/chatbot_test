df206499f72e8f809386adf153e9027efe8b61fb
dawol <dawol2205@gmail.com>
2024-11-22 14:44:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@ import streamlit as st
 from openai import OpenAI
 import numpy as np 
 
-chart_data = pd.DataFrame( np.random.randn(20, 3), 
-                          columns=["a", "b", "c"] ) st.line_chart(chart_data)
-
-
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(
